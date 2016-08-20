901879aed7d8faef7ebe9e9d3d2c7dec240ed050
Vinicius Brochetto <viniciusbrochetto@gmail.com>
2016-08-20 14:15:47 -0300

    Mais coisas random

--- Assets/Scripts/PrototypeControllers.cs
+++ Assets/Scripts/PrototypeControllers.cs
@@ -8,16 +8,16 @@ public class PrototypeControllers : MonoBehaviour
 
     void LateUpdate()
     {
-        //if (Input.GetKeyDown(KeyCode.F1))
-        //{
-        //    ragdolls = !ragdolls;
-        //    GameObject.FindObjectOfType<RagdollController>().SetRagdollActive(ragdolls);
-        //}
-        //if (Input.GetKeyDown(KeyCode.F2))
-        //{
-        //    ik = !ik;
-        //    GameObject.FindObjectOfType<IKController>().ikActive = ik;
-        //}
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            ragdolls = !ragdolls;
+            GameObject.FindObjectOfType<RagdollController>().SetRagdollActive(ragdolls);
+        }
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            ik = !ik;
+            GameObject.FindObjectOfType<IKController>().ikActive = ik;
+        }
         if (Input.GetKeyDown(KeyCode.F3))
         {
             GameObject.FindObjectOfType<CameraShake>().RequestShake();
